0ac6638dd12fb0170f352a5aa6f372d6a66d3269
Arjun Chandarana <achandarana801@gmail.com>
2021-04-05 17:50:17 +0530

    AngularTestingAssignment5

---
+++ AngularTestingAssignment5/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { CounterComponent } from './counter/counter.component';
+import { LightswitchComponent } from './lightswitch/lightswitch.component';
+import { CheckNumberComponent } from './check-number/check-number.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculatorComponent,
+    CounterComponent,
+    LightswitchComponent,
+    CheckNumberComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
